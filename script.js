cb407ca0826b49bfe6d0789b16de398e2724d1ea
Marco Bacci <marcobacci94@gmail.com>
2025-11-06 16:59:16 +0100

    Feat: add try and catch

--- script.js
+++ script.js
@@ -5,28 +5,35 @@ async function fetchJson(url) {
 }
 
 async function getDashboardData(query) {
-  const destinationsProm = fetchJson(
-    `http://localhost:3333/destinations/?search=${query}`
-  );
-  const weathersProm = fetchJson(
-    `http://localhost:3333/weathers?search=${query}`
-  );
-  const airportsProm = fetchJson(
-    `http://localhost:3333/airports?search=${query}`
-  );
+  try {
+    const destinationsProm = fetchJson(
+      `http://localhost:3333/destinations/?search=${query}`
+    );
+    const weathersProm = fetchJson(
+      `http://localhost:3333/weathers?search=${query}`
+    );
+    const airportsProm = fetchJson(
+      `http://localhost:3333/airports?search=${query}`
+    );
 
-  const [destinations, weathers, airports] = await Promise.all([destinationsProm, weathersProm, airportsProm]);
+    const [destinations, weathers, airports] = await Promise.all([
+      destinationsProm,
+      weathersProm,
+      airportsProm,
+    ]);
 
- 
-  return {
-    city: destinations[0].name,
-    country: destinations[0].country,
-    temperature: weathers[0].temperature,
-    weather: weathers[0].weather_description,
-    airport: airports[0].name,
-  };
+    return {
+      city: destinations[0].name,
+      country: destinations[0].country,
+      temperature: weathers[0].temperature,
+      weather: weathers[0].weather_description,
+      airport: airports[0].name,
+    };
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-getDashboardData("London").then((data) => {
-  console.log("data:", data);
+getDashboardData("London").then((info) => {
+  console.log("info:", info);
 });
