6ca34118a684bedaf60c19d9cca94bbb04ebccc1
Marco Bacci <marcobacci94@gmail.com>
2025-11-06 16:49:40 +0100

    Feat: create function to find query

---
+++ script.js
@@ -0,0 +1,35 @@
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = await response.json();
+  return obj;
+}
+
+async function getDashboardData(query) {
+  const destinationsProm = fetchJson(
+    `http://localhost:3333/destinations/?search=${query}`
+  );
+  const weathersProm = fetchJson(
+    `http://localhost:3333/weathers?search=${query}`
+  );
+  const airportsProm = fetchJson(
+    `http://localhost:3333/airports?search=${query}`
+  );
+
+  const results = Promise.all([destinationsProm, weathersProm, airportsProm]);
+
+  const destinations = results[0];
+  const weathers = results[1];
+  const airports = results[2];
+
+  return {
+    city: destinations[0].name,
+    country: destinations[0].country,
+    temperature: weathers[0].temperature,
+    weather: weathers[0].weather_description,
+    airport: airports[0].name,
+  };
+}
+
+// getDashboardData("London").then((data) => {
+//   console.log("data:", data);
+// });
